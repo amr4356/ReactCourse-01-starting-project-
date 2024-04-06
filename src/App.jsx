7e71c5ed79493b618365b5a252f25c73e8c17306
amr4356 <amrooo73321@gmail.com>
2024-04-07 00:32:18 +0300

    48. Component Composition: The special "children" Prop [Core Concept]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header.jsx';
 import {CORE_CONCEPTS} from './data.js';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -27,6 +28,16 @@ function App() {
             />
             
           </ul>
+        </section>
+
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
         
       </main>
