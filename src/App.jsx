3547efd253420ebec4f77fd4a9a490bacfbac9d3
amr4356 <amrooo73321@gmail.com>
2024-04-06 19:33:42 +0300

    46. Best Practice: Storing Components in Files & Using a Good Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
+import Header from './components/Header.jsx';
 import {CORE_CONCEPTS} from './data.js';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriotions=["Fundamental",'Crucial','Core'];
-function genRandoInt(max){
-  return Math.floor(Math.random() * (max+1));
-}
-
-
-function Header(){
-  const description= reactDescriotions[genRandoInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CorConcept({title,description,image}){
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -39,18 +11,18 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CorConcept 
+            <CoreConcept 
               title={CORE_CONCEPTS[0].title} 
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CorConcept 
+            <CoreConcept 
               {...CORE_CONCEPTS[1]}
             />
-            <CorConcept 
+            <CoreConcept 
               {...CORE_CONCEPTS[2]}
             />
-            <CorConcept 
+            <CoreConcept 
               {...CORE_CONCEPTS[3]}
             />
             
