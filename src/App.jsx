cce681e3dd980a557f697d46fdca53d1a5f86eaf
amr4356 <amrooo73321@gmail.com>
2024-04-08 16:57:16 +0300

    55. Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { EXAMPLES } from './data.js';
 
 
 function App() {
-  const [selectedTopic,setSelectedTopic] = useState('components');
+  const [selectedTopic,setSelectedTopic] = useState();
 
   function handleSelect(selectedButton){
     // selected button => 'components', 'jsx'....
@@ -17,6 +17,19 @@ function App() {
   }
 
   console.log('App Component Executing');
+
+  let tabContent=<p>Please select a topic.</p>;
+  if(selectedTopic){
+    tabContent=(
+      <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+      </div>
+    );
+  }
 
 
   return (
@@ -52,15 +65,20 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
+            {/* {!selectedTopic ? (
+               <p>Please select a topic.</p>
+            ) : (
+              <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+              </div>
+            )} */}
+      
         </section>
-        
       </main>
     </div>
   );
